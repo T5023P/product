37ebf85e790aa6ea3984860ea82da8c984210a86
Arsh Siddiqui <arshsiddiqui7609@gmail.com>
2026-06-03 14:41:42 +0530

    Restore admin icon font styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,10 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
+          rel="stylesheet"
+        />
       </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
